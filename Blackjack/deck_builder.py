c7d5b581be9e09e384e189e8e6b9b2d5852c476d
mwilliams70 <templehillride@gmail.com>
2022-11-11 22:35:50 -0500

    comments added

--- Blackjack/deck_builder.py
+++ Blackjack/deck_builder.py
@@ -4,17 +4,12 @@ SUITS=["H", "C", "D", "S"]
 NUMBERS=[2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
 
 def deck_builder(deck_list):
+    #Goes through each suit, {Hearts, Clubs, Diamonds, Spades}
     for suit in SUITS:
+        #Goes through each number {2-10, then Jack, Queen, King, Ace}
         for number in NUMBERS:
+            #makes a Card with each suit and number
             c=card.Card(number, suit)
+
+            #card is added to the deck
             deck_list.append(c)
-
-def main():
-    deck=[]
-    deck_builder(deck)
-    i=0
-    while i < len(deck):
-        print(str(i) + ": " + str(deck[i]))
-        i+=1
-
-main()
