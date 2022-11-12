ddd659b18460651aa0ce59b267419055837dbe52
mwilliams70 <templehillride@gmail.com>
2022-11-11 22:53:56 -0500

    Process of dealing and calculating total is starting to be implemented

---
+++ Blackjack/blackjack.py
@@ -0,0 +1,35 @@
+import deck_builder as db
+import random
+
+def main():
+    deck=[]
+    db.deck_builder(deck)
+    # random.shuffle(deck)
+    print(deck)
+    c1=deck.pop(len(deck)-1)
+    c2=deck.pop(len(deck)-1)
+    print("Your cards are: " + str(c1) + " and " + str(c2) + "\n")
+
+    # Gets the value of the two cards 
+    number1=c1.get_number()
+    number2=c2.get_number()
+
+    """
+    Checks for the Cards value, If it is {J, Q, or K} the value will be set to 10
+    If the value is 'A', the value is set to 11
+    """
+    if c1.get_number() == "J" or c1.get_number() == "Q" or c1.get_number() == "K":
+        number1=10
+    if c2.get_number() == "J" or c2.get_number() == "Q" or c2.get_number() == "K":
+        number2=10
+    if c1.get_number() == "A" or c2.get_number() == "A":
+        number1=11
+        number2=11
+
+    total = int(number1)+int(number2)
+    print("Your total is: " + str(total))
+
+    print(deck)
+    
+
+main()
