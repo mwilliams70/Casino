560cc6b25e7b6bc9b7c50a4c48438f72cfa3700d
mwilliams70 <templehillride@gmail.com>
2022-11-11 22:34:08 -0500

    52 Card deck works now

--- Blackjack/deck_builder.py
+++ Blackjack/deck_builder.py
@@ -1,7 +1,7 @@
 import card
 
 SUITS=["H", "C", "D", "S"]
-NUMBERS=[1,2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+NUMBERS=[2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
 
 def deck_builder(deck_list):
     for suit in SUITS:
@@ -9,3 +9,12 @@ def deck_builder(deck_list):
             c=card.Card(number, suit)
             deck_list.append(c)
 
+def main():
+    deck=[]
+    deck_builder(deck)
+    i=0
+    while i < len(deck):
+        print(str(i) + ": " + str(deck[i]))
+        i+=1
+
+main()
